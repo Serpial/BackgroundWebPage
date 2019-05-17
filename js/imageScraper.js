e25f0297faad523c27f23d68fe90b26d5ba123d0
serpial <phutchison8@gmail.com>
2019-05-17 13:32:02 +0100

    Finished version 1 of background switching

--- js/imageScraper.js
+++ js/imageScraper.js
@@ -1,4 +1,5 @@
-function getImage(document) {
+
+function setBackground(document) {
     const sourceWebsite = "https://www.pexels.com/";
     const sourceForImages = sourceWebsite + "search/desktop%20wallpaper/";
     
@@ -13,12 +14,61 @@ function getImage(document) {
 
 function pickImage(imageSources) {
     
+    /*
     // Now to randomly select one using an anchor like the date.
     currentDate = new Date();
     var daysInMonth = getDaysInMonth(currentDate.getMonth(),
                                      currentDate.getYear());
+    */
+
+    // For now we are just going to pick the first one
+    var imageSource = imageSources[Math.floor((Math.random() * 10))];
+    var creditName = "", creditLink = "", image = "";
     
     
+    console.log("Chosen source : " + imageSource);
+    
+    $.getJSON('http://www.whateverorigin.org/get?url=' + encodeURIComponent(imageSource) + '&callback=?', function(data){
+        // Get image
+        image = getImage(data);
+
+        $(".form__background--image").attr("src", image);
+
+        creditLink = imageSource;
+        creditName = getCredit(data);
+
+        
+        $(".footer--link").attr("href",creditLink);
+        $(".footer--link").text(creditName);
+        
+    });
+    
+}
+
+function getCredit(data) {
+    const nameTag = "js-photo-page-mini-profile-full-name photo-page__mini-profile__text__title";
+    const start = data.contents.indexOf(nameTag) + nameTag.length+2;
+    return data.contents.substring(
+        start,
+        data.contents.indexOf("<", start)
+    );
+
+}
+
+function getImage(data) {
+    const imageClass = "js-photo-page-image-img";
+
+    var imageElement = data.contents.substring(
+        data.contents.indexOf(imageClass) - 12,
+        data.contents.indexOf(">", data.contents.indexOf(imageClass) - 12)
+    );
+    var image = imageElement.substring(
+        imageElement.indexOf("src='")+ "src='".length,
+        imageElement.indexOf("'",imageElement.indexOf("src='")+ "src='".length+1)
+    );
+    
+    console.log("Chosen image : " + image);
+    return image;
 }
 
 function getListOfImageSources(data, sourceWebsite, sourceForImages) {
